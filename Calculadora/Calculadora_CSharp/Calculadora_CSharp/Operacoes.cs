5198072fc767fe428262bdbe8b2588b547ef0c0a
Diego de Souza <49752675+Diego-de-Souza@users.noreply.github.com>
2023-08-21 20:51:26 -0300

    first commit with the first program

---
+++ Calculadora/Calculadora_CSharp/Calculadora_CSharp/Operacoes.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Numerics;
+
+namespace Calculadora_CSharp
+{
+    class Operacoes
+    {
+        public Double ResultadoOperacao;
+
+        //operações da calculadora
+        //operação de soma
+        public void Somar(Double x, Double y)
+        {
+            this.ResultadoOperacao = x + y;
+            Console.WriteLine(ResultadoOperacao);
+        }
+        //operação de subtração
+        public void Subtrair(Double x, Double y)
+        {
+            this.ResultadoOperacao = x - y;
+            Console.WriteLine(ResultadoOperacao);
+        }
+        //operação de divisão
+        public void Dividir(Double x, Double y)
+        {
+            this.ResultadoOperacao = x / y;
+            Console.WriteLine(ResultadoOperacao);
+        }
+        //operação de multiplicação
+        public void Multiplicar(Double x, Double y)
+        {
+            this.ResultadoOperacao = x * y;
+            Console.WriteLine(ResultadoOperacao);
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Diversidade_em_linguagens
+
+## Este repositório é focado em mostrar a diferença de sintaxe em linguagens de programação diferentes, Não irá abranger todas as linguagens existente, mas as mais utilizadas e conhecidas ultimamente.
+
